bb19695a78e73c155a6dbfa3934f3d763ac97499
YamannVinci <yamannvinci@gmail.com>
2023-02-04 02:06:01 +0530

    Update CS-App.py

--- CS-App.py
+++ CS-App.py
@@ -2,28 +2,30 @@ import streamlit as st
 import pickle
 import pandas as pd
 from PIL import Image
+
 im = Image.open("Flowchart-1.png")
 st.image(im, width = 700, caption = "Methodology")
-
 
 html_temp = """
 <div style="background-color:blue;padding:1.5px">
 <h1 style='color:white;text-align:center;">Compressive Strength Predicition </h1>
 </div><br>"""
 st.markdown(html_temp,unsafe_allow_html=True)
+
 st.sidebar.title('Enter the Following Parameters')
-RH=st.sidebar("Rebound Number", 14, 55.50, step=10)
-UPV=st.sidebar("Ultrasonic Pulse Velocity", 1.82, 5.22, step=10)
+RH = st.sidebar.slider("Rebound Number", 14, 55.50, step=0.1)
+UPV = st.sidebar.slider("Ultrasonic Pulse Velocity", 1.82, 5.22, step=0.1)
 
 def csMPa():
     my_dict = {"RH" : RH,
               "UPV" : UPV}
-    df_sample = pd.DataFrame.from_dict(my_dict)
+    df_sample = pd.DataFrame.from_dict(my_dict, orient='index').T
     return df_sample
+
 dfc = csMPa()
-model = pickle.load(open("model_xg", "rb"))
+model = pickle.load(open("model_xg.pkl", "rb"))
 
 if st.sidebar.button("Submit"):
-    result = (model.predict(dfc))
+    result = model.predict(dfc)
     st.success(f"Compressive Strength Prediction of the Concrete is {result[0]} MPa")
   
